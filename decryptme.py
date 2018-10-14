7d56a53a7843434472d1335eec2a0d000622b682
0xShaolin <odinsecurity@protonmail.com>
2018-10-13 21:20:30 -0500

    Initial Commit

--- decryptme.py
+++ decryptme.py
@@ -3,6 +3,7 @@ try:
 	import string
 	import base64
 	import sys
+	import os
 except ImportError:
 	exit(1)
 
@@ -15,6 +16,36 @@ def rot13_text():
 def base64_text():
 	plaintext = base64.b64decode(cipher)
 	print plaintext
+	exit(1)
+
+def rot13_file():
+	rot13 = string.maketrans("ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz", "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
+	try:
+		c = open(cipher, 'rb')
+	except IOError:
+		print "File not found."
+		exit(0)
+	ciphertext = c.read()
+	plaintext = string.translate(ciphertext, rot13)
+	c.close()
+	os.remove(cipher)
+	c = open(cipher, 'w+')
+	c.write(plaintext)
+	print "File overwritten with plaintext."
+	exit(1)
+
+def base64_file():
+	try:
+		c = open(cipher, 'rb')
+	except IOError:
+		print "File not found."
+		exit(0)
+	ciphertext = c.read()
+	plaintext = base64.b64decode(ciphertext)
+	c.close()
+	c = open(cipher, 'w+')
+	c.write(plaintext)
+	print "File overwritten with plaintext."
 	exit(1)
 
 #creating vars and handling exceptions
